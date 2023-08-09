b026fe059a9778646a1bfb524b8328794e722b7a
Denys Filichkin <denys.philichkin@gmail.com>
2023-08-09 16:25:28 +0300

    let's start do it

---
+++ src/database/connector.py
@@ -0,0 +1,25 @@
+from dotenv import dotenv_values
+
+from fastapi import HTTPException, status
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+
+config = dotenv_values(".env")
+SQLALCHEMY_DATABASE_URL = config.get('DATABASE_URL')
+# SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://postgres:567234@195.201.150.230:5433/denis_fill_fa'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DBSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Dependency
+def get_db():
+    db = DBSession()
+    try:
+        yield db
+    except SQLAlchemyError as err_sql:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err_sql))
+    finally:
+        db.close()
+
---
+++ src/repository/notes.py
@@ -0,0 +1,39 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import and_
+
+from src.database.models import Note, User
+from src.schemas import NoteModel
+
+
+async def create(body: NoteModel, db: Session):
+    note = Note(**body.model_dump())
+    db.add(note)
+    db.commit()
+    db.refresh(note)
+    return note
+
+
+async def get_all(user: User, db: Session):
+    notes = db.query(Note).filter(Note.user_id == user.id).all()
+    return notes
+
+
+async def get_one(note_id, user: User, db: Session):
+    note = db.query(Note).filter(and_(Note.user_id == user.id, id=note_id)).first()
+    return note
+
+
+async def update(note_id, body: NoteModel, user: User, db: Session):
+    note = await get_one(note_id, user, db)
+    if note:
+        note.text = body.text
+        db.commit()
+    return note
+
+
+async def delete(note_id, user: User, db: Session):
+    note = await get_one(note_id, user, db)
+    if note:
+        db.delete(note)
+        db.commit()
+    return note
