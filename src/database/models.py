f47facf06477af1b078cd1bdd7f09083e24b2542
Denys Filichkin <denys.philichkin@gmail.com>
2023-08-09 17:31:43 +0300

    edit column change mappeb_column

--- src/database/models.py
+++ src/database/models.py
@@ -1,36 +1,38 @@
-from sqlalchemy import Column, ForeignKey, String, Integer, DateTime, func
-from sqlalchemy.orm import relationship, declarative_base
+from datetime import date
+
+from sqlalchemy import ForeignKey, String, Integer, DateTime, func
+from sqlalchemy.orm import relationship, declarative_base, Mapped, mapped_column
 
 Base = declarative_base()
 
 
 class Contact(Base):
     __tablename__ = 'contacts'
-    id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, index=True)
-    last_name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    phone = Column(String, index=True)
-    birthday = Column(DateTime)
-    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
-    user = relationship('User', backref='contacts')
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(String, index=True)
+    last_name: Mapped[str] = mapped_column(String, index=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    phone: Mapped[int] = mapped_column(String, index=True)
+    birthday: Mapped[date] = mapped_column(DateTime)
+    user_id: Mapped[int] = mapped_column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user: Mapped[str] = relationship('User', backref='contacts')
 
 
 class Note(Base):
     __tablename__ = 'notes'
-    id = Column(Integer, primary_key=True, index=True)
-    contact_id = Column(Integer, ForeignKey('contacts.id'), nullable=False)
-    text = Column(String)
-    contact = relationship("Contact", backref='notes')
-    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
-    user = relationship('User', backref='notes')
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    contact_id: Mapped[int] = mapped_column(Integer, ForeignKey('contacts.id'), nullable=False)
+    text: Mapped[str] = mapped_column(String)
+    contact: Mapped[str] = relationship("Contact", backref='notes')
+    user_id: Mapped[int] = mapped_column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user: Mapped[str] = relationship('User', backref='notes')
 
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(40), nullable=False)
-    email = Column(String(120), unique=True, nullable=False, index=True)
-    password = Column(String(255), nullable=False)
-    created_at = Column('created_at', DateTime, default=func.now())
-    refresh_token = Column(String(255), nullable=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False, index=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now())
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
